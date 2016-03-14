6c59c22c03df5c1cb32f526eadee7ff33d9fd7a7
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-03-14 17:09:16 +0100

    Creation of a main in module tlsremote

--- pelix/shell/tlsremote.py
+++ pelix/shell/tlsremote.py
@@ -201,3 +201,103 @@ class TLS_IPopoRemoteShell(IPopoRemoteShell):
 
 
 # ------------------------------------------------------------------------------
+
+
+def main(address="localhost", port=9001):
+    """
+    Starts a framework with a remote shell and starts an interactive console.
+
+    :param address: Shell binding address
+    :param port: Shell binding port
+    """
+    from pelix.ipopo.constants import use_ipopo
+    import pelix.framework
+
+    # Start a Pelix framework
+    framework = pelix.framework.create_framework(('pelix.ipopo.core',
+                                                  'pelix.shell.core',
+                                                  'pelix.shell.ipopo',
+                                                  'pelix.shell.tlsremote'))
+    framework.start()
+    context = framework.get_bundle_context()
+
+    # Instantiate a Remote Shell
+    with use_ipopo(context) as ipopo:
+        # TODO: use contant (for factory name)
+        rshell = ipopo.instantiate("ipopo-tlsremote-shell-factory",
+                                   "remote-shell",
+                                   {"pelix.shell.tlsremote.address": address,
+                                    "pelix.shell.tlsremote.port": port,
+                                    "pelix.shell.tlsremote.sslversion": ssl.PROTOCOL_TLSv1})
+    # Prepare interpreter variables
+    variables = {'__name__': '__console__',
+                 '__doc__': None,
+                 '__package__': None,
+                 'framework': framework,
+                 'context': context,
+                 'use_ipopo': use_ipopo}
+
+    # Prepare a banner
+    host, port = rshell.get_access()
+    banner = "{lines}\nPython interpreter with Pelix TLS Remote Shell\n" \
+        "Remote shell bound to: {host}:{port}\n{lines}\n" \
+        "Python version: {version}\n" \
+        .format(lines='-' * 80, version=sys.version,
+                host=host, port=port)
+
+    try:
+        # Run an interpreter
+        _run_interpreter(variables, banner)
+    finally:
+        # Stop the framework
+        framework.stop()
+
+
+def _run_interpreter(variables, banner):
+    """
+    Runs a Python interpreter console and blocks until the user exits it.
+
+    :param variables: Interpreters variables (locals)
+    :param banner: Start-up banners
+    """
+    # Script-only imports
+    import code
+    try:
+        import readline
+        import rlcompleter
+        readline.set_completer(rlcompleter.Completer(variables).complete)
+        readline.parse_and_bind("tab: complete")
+
+    except ImportError:
+        # readline is not available: ignore
+        pass
+
+    # Start the console
+    shell = code.InteractiveConsole(variables)
+    shell.interact(banner)
+
+# ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    # Prepare arguments
+    import argparse
+    parser = argparse.ArgumentParser(description="Pelix Remote Shell")
+    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
+                        help="Set loggers at debug level")
+    parser.add_argument("-a", "--address", dest="address", default="localhost",
+                        help="The remote shell binding address")
+    parser.add_argument("-p", "--port", dest="port", type=int, default=9001,
+                        help="The remote shell binding port")
+    # TODO: Add argument for TLS version
+
+    # Parse them
+    args = parser.parse_args()
+
+    # Prepare the logger
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    # Run the entry point
+    main(args.address, args.port)
