bb889cd4ae9a69d5ec3e082264bff35df6031e59
Abdelaziz Founas <founasabd@gmail.com>
2016-03-14 15:22:56 +0100

    Initial commit of keystone

---
+++ pelix/shell/crypto/keystore.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -- Content-Encoding: UTF-8 --
+"""
+iPOPO keystore
+
+Provides a keystore interface
+
+:author: Abdelaziz Founas
+"""
+
+# Standard library
+import logging
+
+_logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+
+
+class KeyStore:
+    def __init__(self, aPath):
+        self.pPath = aPath
+
+    def getPath():
+        pass
+
+    def addCert(aCert):
+        pass
+
+    def getCert(aId):
+        pass
+
+    def removeCert(aId):
+        pass
+
+    def createKeystore(aPath, aInfos):
+        pass
