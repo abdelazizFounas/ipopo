37c5a7a6cb04a00690ab983e5546878f40b07c46
Abdelaziz Founas <founasabd@gmail.com>
2016-03-21 10:56:45 +0100

    Changement import pour le test du BasicKeyStore

--- tests/crypto/test_basickeystore.py
+++ tests/crypto/test_basickeystore.py
@@ -5,7 +5,7 @@ Tests the basic keystore
 """
 
 # Pelix
-from pelix.crypto import SERVICE_CRYPTO, SERVICE_CRYPTO_BASICKEYSTORE
+from pelix.crypto.basickeystore import BasicKeyStore
 
 # Standard library
 import threading
