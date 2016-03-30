c7b61feba86e94c1ee3622476a4b8c03edd8f4c8
Marwan Hallal <marwanh@users.noreply.github.com>
2016-03-30 11:35:47 +0200

    move entity class to another file

--- pelix/crypto/certificate.py
+++ pelix/crypto/certificate.py
@@ -8,59 +8,6 @@ __version__ = ".".join(str(x) for x in __version_info__)
 # Documentation strings format
 __docformat__ = "restructuredtext en"
 
-class Entity:
-
-	def __init__(self, countryName="", stateOrProvinceName="", localityName="", organizationName="", organizationalUnitName="", commonName="", email=""):
-		self.C = countryName
-		self.ST = stateOrProvinceName
-		self.L = localityName
-		self.O = organizationName
-		self.OU = organizationalUnitName
-		self.CN = commonName
-		self.emailAddress = email
-
-	def get_countryName(self):
-		return self.C
-
-	def get_stateOrProvinceName(self):
-		return self.ST
-
-	def get_localityName(self):
-		return self.L
-
-	def get_organizationName(self):
-		return self.O
-
-	def get_organizationalUnitName(self):
-		return self.OU
-
-	def get_commonName(self):
-		return self.CN
-
-	def get_emailAddress(self):
-		return self.emailAddress
-
-	def set_countryName(self, countryName):
-		self.C = countryName
-
-	def set_stateOrProvinceName(self, stateOrProvinceName):
-		self.ST = stateOrProvinceName
-
-	def set_localityName(self, localityName):
-		self.L = localityName
-
-	def set_organizationName(self, organizationName):
-		self.O = organizationName
-
-	def set_organizationalUnitName(self, organizationalUnitName):
-		self.OU = organizationalUnitName
-
-	def set_commonName(self, commonName):
-		self.CN = commonName
-
-	def set_emailAddress(self, email):
-		self.emailAddress = email
-
 
 class Certificate:
 
