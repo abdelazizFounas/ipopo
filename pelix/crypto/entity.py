d48c7cf054c87cc38ea8d399f6768906c61b667f
Marwan Hallal <marwanh@users.noreply.github.com>
2016-04-04 18:16:58 +0200

    Document Entity module

--- pelix/crypto/entity.py
+++ pelix/crypto/entity.py
@@ -10,10 +10,24 @@ __docformat__ = "restructuredtext en"
 
 
 class Entity:
+    """
+    Represents a certification entity (either an issuer or a certification subject)
+    """
 
     def __init__(self, countryName="", stateOrProvinceName="", localityName="",
                  organizationName="", organizationalUnitName="",
                  commonName="", email=""):
+        """
+        Sets up the entity
+
+        :param countryName
+        :param stateOrProvinceName
+        :param localityName
+        :param organizationName
+        :param organizationalUnitName
+        :param commonName
+        :param email
+        """ 
         self.C = countryName
         self.ST = stateOrProvinceName
         self.L = localityName
@@ -23,43 +37,85 @@ class Entity:
         self.emailAddress = email
 
     def get_countryName(self):
+        """
+        Returns the country name
+        """
         return self.C
 
     def get_stateOrProvinceName(self):
+         """
+        Returns the state or province name
+        """
         return self.ST
 
     def get_localityName(self):
+         """
+        Returns the locality name
+        """
         return self.L
 
     def get_organizationName(self):
+        """
+        Returns the organization name
+        """
         return self.O
 
     def get_organizationalUnitName(self):
+        """
+        Returns the organizational unit name
+        """
         return self.OU
 
     def get_commonName(self):
+        """
+        Returns the common name
+        """
         return self.CN
 
     def get_emailAddress(self):
+        """
+        Returns the email address
+        """
         return self.emailAddress
 
     def set_countryName(self, countryName):
+        """
+        Sets the country name
+        """
         self.C = countryName
 
     def set_stateOrProvinceName(self, stateOrProvinceName):
+        """
+        Sets the state or province name
+        """
         self.ST = stateOrProvinceName
 
     def set_localityName(self, localityName):
+        """
+        Sets the locality name
+        """
         self.L = localityName
 
     def set_organizationName(self, organizationName):
+        """
+        Sets the organization name
+        """
         self.O = organizationName
 
     def set_organizationalUnitName(self, organizationalUnitName):
+        """
+        Sets the organizational unit name
+        """
         self.OU = organizationalUnitName
 
     def set_commonName(self, commonName):
+        """
+        Sets the common name
+        """
         self.CN = commonName
 
     def set_emailAddress(self, email):
+        """
+        Sets the email address
+        """
         self.emailAddress = email
