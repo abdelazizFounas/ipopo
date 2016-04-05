13a8c38cfe03f35ec84df20f554b8c47694fdcc6
Abdelaziz Founas <founasabd@gmail.com>
2016-04-05 12:22:32 +0200

    Fixed dump and load in BasicKeyStore

--- tests/crypto/test_basickeystore.py
+++ tests/crypto/test_basickeystore.py
@@ -6,6 +6,8 @@ Tests the basic keystore
 
 # Pelix
 from pelix.crypto.basickeystore import BasicKeyStore
+from pelix.crypto.certificate import Certificate
+from pelix.crypto.entity import Entity
 
 # Standard library
 import threading
@@ -20,29 +22,23 @@ __version__ = "1.0.0"
 
 # ------------------------------------------------------------------------------
 
-
-class DummyCertificates(object):
-    """
-    Dummy certificates
-    """
-    def __init__(self):
-        # Topic of the last received event
-        self.data = 'data'
-        self.aId = 'aId'
-    def getId(self):
-        return self.aId
-    def __str__(self):
-        return 'CERTIFICAT : ' + self.data + ' ' + self.aId
-
-# ------------------------------------------------------------------------------
-
-
 class BasicKeystoreTest(unittest.TestCase):
     """
     Tests the basic keystore
     """
     def testBasic(self):
         self.ks = BasicKeyStore('./testBKS')
-        self.cert = DummyCertificates()
+
+        self.assertTrue(os.path.exists('./testBKS'), "Keystore folder was not created")
+
+        self.cert = Certificate()
+        self.ent = Entity("FR", "Drome", "Loriol", "Elipce", "E", "name", "name@name.com")
+        self.cert.set_subject(self.ent)
+
         self.ks.addCert(self.cert)
+
+        self.assertTrue(os.path.exists('./testBKS/'+cert.ID()+'.pem'), "Certificate was not dumped on disk")
+
         self.cert2 = self.ks.getCert(self.cert.getId())
+
+        self.assertTrue(cert.ID() == cert2.ID(), "The retrieved cert is not the same")
