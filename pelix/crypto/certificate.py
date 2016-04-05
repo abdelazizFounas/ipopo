bff6fbecad60ef275778ae466812152cad902f85
Marwan Hallal <marwanh@users.noreply.github.com>
2016-04-05 12:17:05 +0200

    add version and serial to certificates

--- pelix/crypto/certificate.py
+++ pelix/crypto/certificate.py
@@ -36,6 +36,12 @@ class Certificate:
     def get_notBefore(self):
         return self._cert.get_notBefore()
 
+    def get_version(self):
+        return self._cert.get_version()
+
+    def get_serial_number(self):
+        return self._cert.get_serial_number()
+
     def set_subject(self, subject):
         s = self._unwrap_entity_into_x509name(subject, "subject")
         self._cert.set_subject(s)
@@ -46,6 +52,7 @@ class Certificate:
 
     def set_pubkey(self, pubkey):
         self._pubkey = pubkey
+        self._cert.set_pubkey(pubkey._pkey)
 
     def set_notAfter(self, when):
         self._cert.set_notAfter(when)
@@ -53,22 +60,43 @@ class Certificate:
     def set_notBefore(self, when):
         self._cert.set_notBefore(when)
 
+    def set_version(self, version):
+        self._cert.set_version(version)
+
+    def set_serial_number(self, serial):
+        self._cert.set_serial_number(serial) 
+
     def has_expired(self):
         return self._cert.has_expired()
 
     def sign(self, pubkey, digest):
         self._cert.sign(pubkey._pkey, digest)
 
-    def dump_certificate(self):
+    def dump(self):
         return crypto.dump_certificate(crypto.FILETYPE_PEM, self._cert)
 
-
+    def load(buf):
+        cert = Certificate()
+        key = Key()
+        _x509 = crypto.load_certificate(crypto.FILETYPE_PEM, buf)
+        cert._cert = _x509
+        subject = cert._wrap_x509Name_into_Entity("subject")
+        issuer = cert._wrap_x509Name_into_Entity("issuer")
+        key._pkey = _x509.get_pubkey()
+        cert.set_subject(subject)
+        cert.set_issuer(issuer)
+        cert.set_pubkey(key)
+        cert.set_notAfter(_x509.get_notAfter())
+        cert.set_notBefore(_x509.get_notBefore())
+        cert.set_version(_x509.get_version())
+        cert.set_serial_number(_x509.get_serial_number())
+        return cert
 
     def _wrap_x509Name_into_Entity(self, subject_or_issuer):
         if subject_or_issuer == "subject":
-            name = self._cert.get_issuer()
-        elif subject_or_issuer == "issuer":
             name = self._cert.get_subject()
+        elif subject_or_issuer == "issuer":
+            name = self._cert.get_issuer()
 
         c = name.C
         st = name.ST
