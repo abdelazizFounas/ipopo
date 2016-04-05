823667bb902f3726e3021908be524df934d9ce52
Abdelaziz Founas <founasabd@gmail.com>
2016-04-05 16:38:26 +0200

    Fixed the tests on the BasicKeyStore

--- tests/crypto/test_basickeystore.py
+++ tests/crypto/test_basickeystore.py
@@ -8,6 +8,7 @@ Tests the basic keystore
 from pelix.crypto.basickeystore import BasicKeyStore
 from pelix.crypto.certificate import Certificate
 from pelix.crypto.entity import Entity
+from pelix.crypto.key import Key
 
 # Standard library
 import threading
@@ -27,18 +28,43 @@ class BasicKeystoreTest(unittest.TestCase):
     Tests the basic keystore
     """
     def testBasic(self):
+        # Creation of a BasicKeyStore to test it
         self.ks = BasicKeyStore('./testBKS')
 
+        # The path given to the BasicKeyStore should now exist
         self.assertTrue(os.path.exists('./testBKS'), "Keystore folder was not created")
 
+        # Create a certificate object
         self.cert = Certificate()
-        self.ent = Entity("FR", "Drome", "Loriol", "Elipce", "E", "name", "name@name.com")
-        self.cert.set_subject(self.ent)
 
+        # Fill the certificate with not important information (we just need to test the BasicKeyStore)
+        self.key = Key()
+        self.key.generate_key(Key.TYPE_RSA, 1024)
+        self.issuer = Entity("LB", "Hello", "World", "org", "orgunit", "common", "hello.world@gmail.com")
+        self.subject = Entity("FR", "Drome", "Loriol", "Elipce", "E", "name", "name@name.com")
+        self.cert.set_subject(self.subject)
+        self.cert.set_issuer(self.issuer)
+        self.cert.set_pubkey(self.key)
+        self.cert.set_notAfter(b'20170207053015')
+        self.cert.set_notBefore(b'20160207053015')
+        self.cert.set_version(10)
+        self.cert.set_serial_number(12345)
+
+        # Create a new key to sign the certificate with
+        self.key2 = Key()
+        self.key2.generate_key(Key.TYPE_RSA, 1024)
+
+        # Sign the certificate with the latter key
+        self.cert.sign(self.key2, 'sha1')
+
+        # Add the created and filled certificate to the BasicKeyStore
         self.ks.addCert(self.cert)
 
+        # A new file should be present in the path of the BasicKeyStore
         self.assertTrue(os.path.exists('./testBKS/'+cert.ID()+'.pem'), "Certificate was not dumped on disk")
 
+        # Retrive the certificate added to the BasicKeyStore
         self.cert2 = self.ks.getCert(self.cert.getId())
 
+        # The ID of the certificate added to the BasicKeyStore and the one retrieved should be equal
         self.assertTrue(cert.ID() == cert2.ID(), "The retrieved cert is not the same")
